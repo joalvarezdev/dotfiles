d9684bdff58eda128d55ae41bca437e47c5aada0
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-16 18:38:30 -0300

    chore: add new configuration

--- scripts/visualization/zsh-viz.sh
+++ scripts/visualization/zsh-viz.sh
@@ -8,6 +8,13 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+original="~/dotfiles/zsh_config/.zshrc"
+enlace="~/.zshrc"
+
+if [ ! -L "$enlace" ]; then
+	ln -s "$original" "$enlace"
+fi
+
 if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k"]; then
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
