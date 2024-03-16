4b626abc2f27e6d87529d78ba7ea9d4bd02219c8
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-16 18:48:46 -0300

    chore: test 3

--- scripts/visualization/zsh-viz.sh
+++ scripts/visualization/zsh-viz.sh
@@ -8,13 +8,6 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-dot_zsh="~/dotfiles/zsh_config/.zshrc"
-path_final="~/.zshrc"
-
-if [ ! -L "$path_final" ]; then
+if [ ! -L "~/.zshrc" ]; then
 	ln -s "~/dotfiles/zsh_config/.zshrc" "~/.zshrc"
-fi
-
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k"]; then
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
