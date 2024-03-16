66dcd1da2b3e52729ff48c5f9e1f9d8c6f78e7a3
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-16 17:18:20 -0300

    chore: add validation to NVM

--- scripts/configuration/nvm.sh
+++ scripts/configuration/nvm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+if ! command -v nvm &>/dev/null; then
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
