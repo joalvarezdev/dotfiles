b05246fd56ba30dbc491d3fb2bb338cd8310705e
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-18 20:52:14 -0300

    chore: The creation of the symbolic link from an unnecessary file was eliminated

--- scripts/visualization/zsh-viz.sh
+++ scripts/visualization/zsh-viz.sh
@@ -7,7 +7,3 @@ fi
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-if [ ! -L "~/.zshrc" ]; then
-	ln -s "~/dotfiles/zsh_config/.zshrc" "~/.zshrc"
-fi
