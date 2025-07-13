ce85144f4fc5b0af86f7ed905209448595dfa8b1
joalvarez <alavarez.joaquinez@gmail.com>
2025-07-13 16:03:29 -0300

    chore: add rofi configuration

--- config/i3/rofi.sh
+++ config/i3/rofi.sh
@@ -2,12 +2,26 @@
 
 set -e
 
-if [ -f "/usr/share/rofi/themes/rounded-red-dark.rasi" ]; then
+ROFI_THEMES="/usr/share/rofi/themes"
+ROFI_TMP="/tmp/rofi"
+ROFI_CONFIG="$HOME/.config/rofi"
+
+if [ -f "$ROFI_THEMES/rounded-red-dark.rasi" ]; then
   exit 0
 fi
 
 git clone https://github.com/lr-tech/rofi-themes-collection.git /tmp/rofi
 
-sudo cp -r * /tmp/rofi /usr/share/rofi/themes
+sudo cp -r "$ROFI_TMP/themes" /usr/share/rofi/themes
 
-sudo rm -rf /tmp/rofi
+sudo rm -rf $ROFI_TMP
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+INSTALATION_CONF="$(dirname "$SCRIPT_DIR")"
+
+if [ -d $ROFI_CONFIG ]; then
+  rm -rf $ROFI_CONFIG
+fi
+
+ln -sf "$INSTALATION_CONF/i3/config.rasi" $ROFI_CONFIG
