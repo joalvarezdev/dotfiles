9000edcd64d149ae0c4bf857c7fb8ffe15a6fb1f
joalvarez <alavarez.joaquinez@gmail.com>
2025-07-13 13:53:01 -0300

    chore 🧹: add tmux configuration
    Signed-off-by: joalvarez <alavarez.joaquinez@gmail.com>

--- config/tmux/config.sh
+++ config/tmux/config.sh
@@ -8,5 +8,15 @@ fi
 
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [ ! -d "$TPM_DIR" ]; then
-    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
-else
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DOTFILES="$(dirname "$SCRIPT_DIR")"
+
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/"
+
+tmux
+
+"$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh"
