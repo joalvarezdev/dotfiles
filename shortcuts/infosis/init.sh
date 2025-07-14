0754e1d468bd5a807035bef5ccb3b34a112aa8e7
joalvarez <alavarez.joaquinez@gmail.com>
2025-07-14 17:00:28 -0300

    chore: add new shorcuts for infosis

---
+++ shortcuts/infosis/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+alias infs="$HOME/ghq/gitlab.infosisglobal.com"
+alias zh="$HOME/ghq/gitlab.infosisglobal.com/configuration"
+
+alias cp::msg="cp $HOME/ghq/gitlab.infosisglobal.com/desarrollo/zw-msg/*.properties \
+  $HOME/ghq/gitlab.infosisglobal.com/desarrollo/zw-msg/version-zw-msg.xml \
+  $HOME/ghq/gitlab.infosisglobal.com/configuration/zeushome/messages"
+
+alias cp::sec="cp $HOME/ghq/gitlab.infosisglobal.com/desarrollo/Modulo-Seguridad/modulo-seguridad/arbol.xml \
+  $HOME/ghq/gitlab.infosisglobal.com/desarrollo/Modulo-Seguridad/modulo-seguridad/securityModules.xml \
+  $HOME/ghq/gitlab.infosisglobal.com/desarrollo/Modulo-Seguridad/modulo-seguridad/version-modulo-seguridad.xml \
+  $HOME/ghq/gitlab.infosisglobal.com/configuration/zeushome"
