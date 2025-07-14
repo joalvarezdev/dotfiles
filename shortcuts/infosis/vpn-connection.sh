63b703c45f49f23df1a7d97e2bb68466394e06c8
joalvarez <alavarez.joaquinez@gmail.com>
2025-07-14 18:39:32 -0300

    chore: add vpn connection

---
+++ shortcuts/infosis/vpn-connection.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONNECTION="OpenVPN Server"
+PASSWORD="r(6bjuysz:I\\"
+
+if nmcli connection show --active | grep -q "^$CONNECTION"; then
+  nmcli connection down "$CONNECTION"
+else
+  echo "$PASSWORD" | nmcli --ask connection up "$CONNECTION"
+fi
