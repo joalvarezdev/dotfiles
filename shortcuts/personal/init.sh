143520daec3e53574aea11a6a257fb197c2eb9ae
joalvarez <alavarez.joaquinez@gmail.com>
2025-07-15 09:29:05 -0300

    chore 🧹: more and more shortcuts and apps
    Signed-off-by: joalvarez <alavarez.joaquinez@gmail.com>

---
+++ shortcuts/personal/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+alias me="$HOME/ghq/github.com/joalvarezdev"
--- shortcuts/zsh/init.sh
+++ shortcuts/zsh/init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export PATH="/home/joalvarez/.bun/bin:$PATH"
+export PATH="/home/joalvarez/.local/bin:$PATH"
+
 source "$DOTFILES/shortcuts/zsh/aliases.sh"
