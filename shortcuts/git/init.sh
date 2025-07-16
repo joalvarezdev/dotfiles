680b583bfba0c924f38fc8b7f62858895975e1d8
joalvarez <alvarez.joaquinez@gmail.com>
2025-07-16 19:08:46 -0300

    chore 🧹: add new shortcuts for git
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/git/init.sh
+++ shortcuts/git/init.sh
@@ -7,3 +7,15 @@ alias gcmsg="git commit -m"
 alias gsts="git stash push -m"
 alias gstsu="git stash push -u -m"
 alias gstl="git stash list"
+
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "🚧 --wip-- [skip ci]"'
+alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
+alias gsts='git stash -- $(git diff --staged --name-only)'
+
+gmove() {
+  git stash -- $(git diff --staged --name-only) &&
+    gwip
+  git branch $1 $2 &&
+    git checkout $1 &&
+    git stash pop
+}
