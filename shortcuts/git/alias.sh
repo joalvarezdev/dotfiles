835aef3476498a08e4c07fb25a5ee61ecfdc4a8a
joalvarez <alvarez.joaquinez@gmail.com>
2025-07-18 21:34:20 -0300

    chore 🧹: fix git aliases
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/git/alias.sh
+++ shortcuts/git/alias.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 alias gp="git push"
-alias gpf="git push --force origin $(git branch --show-current)"
-alias gpsup="git push --set-upstream origin $(git branch --show-current)"
+alias gpf="git push --force origin \"$(git branch --show-current)\""
+alias gpsup="git push --set-upstream origin \"$(git branch --show-current)\""
 alias gcmsg="git commit -m"
 alias gsts="git stash push -m"
 alias gstsu="git stash push -u -m"
