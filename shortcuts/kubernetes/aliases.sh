118af8dd12ce82e8dd3d96b366249374b6793465
joalvarez <alvarez.joaquinez@gmail.com>
2025-07-27 20:20:30 -0300

    chore 🧹: add kubectl and kind shortcuts
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/kubernetes/aliases.sh
+++ shortcuts/kubernetes/aliases.sh
@@ -2,3 +2,10 @@
 
 alias k="kubectl"
 alias kc="kconf"
+
+alias k::apl="k apply -k"
+alias k::del="k delete -k"
+alias k::res="k rollout restart"
+
+alias kd::new="kind create cluster --config"
+alias kd::del="kind delete cluster --name"
