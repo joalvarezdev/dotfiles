cfb0e9cd9fc75bc5c9f94c51c57aeb03a340a067
joalvarez <alvarez.joaquinez@gmail.com>
2025-07-30 17:15:02 -0300

    chore 🧹: add new shortcut to create a java monorepository
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/kubernetes/aliases.sh
+++ shortcuts/kubernetes/aliases.sh
@@ -6,6 +6,7 @@ alias kc="kconf"
 alias k::apl="k apply -k"
 alias k::del="k delete -k"
 alias k::res="k rollout restart"
+alias k::del::job="k delete job"
 
 alias kd::new="kind create cluster --config"
 alias kd::del="kind delete cluster --name"
--- shortcuts/personal/init.sh
+++ shortcuts/personal/init.sh
@@ -3,3 +3,4 @@
 alias me="$HOME/ghq/github.com/joalvarezdev"
 
 alias java::api="cookiecutter git@github.com:joalvarezdev/base-java-cookiecutter.git"
+alias java::mono="cookiecutter git@github.com:joalvarezdev/monorepo-java-cookiecutter.git"
