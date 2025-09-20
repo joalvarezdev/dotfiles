f8b9cf0141ab8225dbdc64800070294d2b39d347
joalvarez <alvarez.joaquinez@gmail.com>
2025-09-20 11:50:02 -0300

    feat ✨: improvement to connect vpn
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/infosis/vpn-connection.sh
+++ shortcuts/infosis/vpn-connection.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+source $HOME/dotfiles/shortcuts/bw/functions.sh
+
 CONNECTION="infosis"
-#PASSWORD="r(6bjuysz:I\\"
-PASSWORD='r(6bjuysz:I\'
+PASSWORD=$(load_vpn_pass "1dc3d5bf-cfec-4e32-922d-ad0760fc5238")
 
 if nmcli connection show --active | grep -q "^$CONNECTION"; then
   nmcli connection down "$CONNECTION"
