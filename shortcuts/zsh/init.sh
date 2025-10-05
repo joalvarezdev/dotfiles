3094a81497bcfc6c5ea29538cdc789f93df41ec3
joalvarez <alvarez.joaquinez@gmail.com>
2025-10-05 12:33:40 -0300

    chore 🧹: add fzf keybinding and completions
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/zsh/init.sh
+++ shortcuts/zsh/init.sh
@@ -4,3 +4,7 @@ export PATH="/home/joalvarez/.bun/bin:$PATH"
 export PATH="/home/joalvarez/.local/bin:$PATH"
 
 source "$DOTFILES/shortcuts/zsh/aliases.sh"
+source "/usr/share/fzf/key-bindings.zsh"
+source "/usr/share/fzf/completion.zsh"
+
+source "$DOTFILES/zsh-issues/main.zsh"
