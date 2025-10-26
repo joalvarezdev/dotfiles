293f25548adca5bba32edddda0bab3454b9c7d21
joalvarez <alvarez.joaquinez@gmail.com>
2025-10-26 12:06:27 -0300

    fix 🐛: change into tmux attach windows
    Signed-off-by: joalvarez <alvarez.joaquinez@gmail.com>

--- shortcuts/tmux/tmux-attach.sh
+++ shortcuts/tmux/tmux-attach.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# if [[ -z "$TMUX" ]]; then
-# tmux attach-session -t JoAlvarez || tmux new-session -s JoAlvarez
-# fi
-# Asegurarse de que no estamos ya dentro de tmux
 if [[ -z "$TMUX" ]]; then
-  # Usar exec para reemplazar el proceso bash por tmux
-  exec tmux attach-session -t JoAlvarez || exec tmux new-session -s JoAlvarez
+  tmux attach-session -t JoAlvarez || tmux new-session -s JoAlvarez
 fi
+# Asegurarse de que no estamos ya dentro de tmux
+# if [[ -z "$TMUX" ]]; then
+#   # Usar exec para reemplazar el proceso bash por tmux
+#   exec tmux attach-session -t JoAlvarez || exec tmux new-session -s JoAlvarez
+# fi
